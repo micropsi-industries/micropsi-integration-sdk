e25c270cd1762be4aa73e9f74aaca9e1686ccaa1
Evan Hamilton <evanphamilton@gmail.com>
2021-06-10 15:45:54 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup, Extension
+
+setup(name='integration_sdk',
+      version='0.1',
+      description='integration SDK for micropsi Industries',
+      install_requires=['numpy==1.18.5'],
+      )
