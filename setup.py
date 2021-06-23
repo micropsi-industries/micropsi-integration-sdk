90d6d0cdf406e02074b172d77945d1b7a237995f
Evan Hamilton <evanphamilton@gmail.com>
2021-06-23 15:10:01 +0200

    Update setup.py w/ proper naming.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-setup(name='micropsi_integration_sdk',
+setup(name='integration-sdk',
       version='0.1',
       packages=['micropsi_integration_sdk'],
       package_dir={'micropsi_integration_sdk': './micropsi_integration_sdk'},
