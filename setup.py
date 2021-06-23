fd2b361a031d0f91579e933af7d329a29e23963d
Evan Hamilton <evanphamilton@gmail.com>
2021-06-23 15:17:35 +0200

    Remove redundant line from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from distutils.core import setup, Extension
 setup(name='integration-sdk',
       version='0.1',
       packages=['micropsi_integration_sdk'],
-      package_dir={'micropsi_integration_sdk': './micropsi_integration_sdk'},
       description='integration SDK for micropsi Industries',
       install_requires=['numpy'],
       )
